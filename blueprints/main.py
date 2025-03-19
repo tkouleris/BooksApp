ae68b72d0673325c52100e56cc75410806c8e545
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-03-19 21:28:56 +0200

    webpage

--- blueprints/main.py
+++ blueprints/main.py
@@ -1,8 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory
 
 main = Blueprint('main', __name__)
 
 
-@main.route("/", methods=['GET'])
-def index():
-    return render_template('index.html', )
+# @main.route("/", methods=['GET'])
+# def index():
+#     return render_template('index.html', )
+
+
+@main.route('/', defaults={'path': ''})
+@main.route('/<path:path>')
+def serve_react_app(path):
+    if path != "" and not path.startswith("static") and not path.startswith("api"):
+        return render_template('index.html')
+    else:
+        return send_from_directory('static', path)
