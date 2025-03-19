f21a12f706e8a4ddabd1b489e7e39992e0b15092
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-03-19 20:36:12 +0200

    main

--- blueprints/main.py
+++ blueprints/main.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
+
 @main.route("/", methods=['GET'])
 def index():
-    return render_template('index.html',)
+    return render_template('index.html', )
