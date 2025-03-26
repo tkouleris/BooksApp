da3bba91196a2bed3ca9c3d485bed510d7a6e5ff
Thodoris Kouleris <kurokokoro@windowslive.com>
2025-03-26 20:17:51 +0200

    fix: url

--- blueprints/main.py
+++ blueprints/main.py
@@ -3,17 +3,7 @@ from flask import Blueprint, render_template, send_from_directory
 main = Blueprint('main', __name__)
 
 
-# @main.route("/", methods=['GET'])
-# def index():
-#     return render_template('index.html', )
-
-
 @main.route('/', defaults={'path': ''})
 @main.route('/<path:path>')
 def serve_react_app(path):
     return render_template('index.html')
-    # print(path != "" and not path.startswith("static") and not path.startswith("api"))
-    # if path != "" and not path.startswith("static") and not path.startswith("api"):
-    #     return render_template('index.html')
-    # else:
-    #     return send_from_directory('static', path)
